5d3a41987edbc5d8c53b69af9dfc5dae62a38844
kinnari <kinnari1022@yahoo.com>
2021-04-14 17:11:49 -0400

    third commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk' //has to do w async requests
+import { Provider } from 'react-redux' //need to wrap app in it so it will ahve access to store
 import App from './App';
 
 
